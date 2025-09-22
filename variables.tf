07749c8f8d4783b2b3ae610b90a768a397a28522
joz-incubator <gudauskas.juozas@gmail.com>
2025-09-22 19:00:00 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,32 @@
+
+variable "region" {
+  default = "europe-west6"
+}
+
+variable "zone" {
+  default = "europe-west6-c"
+}
+
+variable "customer" {
+  default = "whatever3"
+}
+
+variable "cidr" {
+  default = "10.0.0.0/24"
+}
+
+variable "fwdestnets" {
+  default = ["18.193.43.69/32"]
+}
+
+variable "fwdestcount" {
+  default = 1
+}
+
+variable "vmtype" {
+  default = "e2-medium"
+}
+
+variable "vmimage" {
+  default = "projects/ubuntu-os-cloud/global/images/ubuntu-2504-plucky-amd64-v20250701"
+}
